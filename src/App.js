8700e04725fc536f3fae21438f86b177e8496628
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2020-12-02 06:50:06 +0530

    google-auth

--- src/App.js
+++ src/App.js
@@ -89,6 +89,27 @@ const SignIn = (props) => {
     });
   }
 
+
+  function onClickGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -139,6 +160,14 @@ const SignIn = (props) => {
           >
             Sign In
           </Button>
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={onClickGoogle}
+            color="secondary"
+          >
+            Google
+          </Button><br /><br />
           <Grid container>
             <Grid item xs>
               <Link href="#" variant="body2">
--- src/SignupScreen.js
+++ src/SignupScreen.js
@@ -20,10 +20,6 @@ import { BrowserRouter, Route,Switch} from 'react-router-dom';
 import { useHistory } from 'react-router';
 import Dashboard from './Dashboard';
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -64,6 +60,13 @@ const SignupScreen = (props) => {
   const [name, setName] = useState("");
   const history = useHistory();
 
+  useEffect(() => {
+    const user = firebase.auth();
+    firebase.auth().onAuthStateChanged(user => {
+      history.push(user ? '/dashboard' : '/signup');
+    })
+  }, []);
+
   function onClick() {
     console.log(email, password);
     firebase
@@ -78,6 +81,26 @@ const SignupScreen = (props) => {
     })
     .catch(e => {
       console.log("error message", e.message);
+    });
+  }
+
+  function onClickGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
     });
   }
 
@@ -144,6 +167,15 @@ const SignupScreen = (props) => {
           >
             Sign Up
           </Button>
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={onClickGoogle}
+            color="secondary"
+          >
+            Google
+          </Button>
+          <br /><br />
           <Grid container>
             <Grid item xs>
               <Link href="#" variant="body2">
